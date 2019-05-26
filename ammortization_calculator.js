4164f746dce7b04821076fc78697492a81b8cf18
praveen <contact@praveenmuthu.dev>
2019-05-26 18:00:21 +1000

    completed schedule and initial commits with chart

--- ammortization_calculator.js
+++ ammortization_calculator.js
@@ -1,19 +1,29 @@
+function getData() {
+  return {
+    presentValue: Number(document.querySelector("#presentValue").value),
+    interestRate: Number(document.querySelector("#interestRate").value),
+    loanTerm:  Number(document.querySelector("#loanTerm").value),
+    compoundingPeriods: 12,
+  }
+}
+
 function calculate() {
-  let presentValue = Number(document.querySelector("#presentValue").value);
-  let interestRate = Number(document.querySelector("#interestRate").value);
-  let loanTerm = Number(document.querySelector("#loanTerm").value);
   // Annuity Payment of a loan = PV  / [(1-(1+r)^-n)/r]
   // PMT = 500,000 / [(1-((1+0.002991666667)^(-30*12))) / (0.002991666667)]
   // PMT = 500,000 / [0.6588360732 / (0.002991666667)]
   // PMT = 500,000 / 220.223757034
-  // PMT = 2,270.4180817459
-  let compounding_periods = 12;
-  let mathInterestRate = (interestRate / 100) / compounding_periods;
-  let interestOne = 1 + mathInterestRate;
-  var demoninator = (1 - Math.pow(interestOne, -loanTerm*compounding_periods))/(mathInterestRate);
-  var resultantValue = presentValue / demoninator;
+  var data = getData();
+  let p = data.presentValue;
+  let apr = data.interestRate;
+  let c = data.compoundingPeriods;
+  let n = data.loanTerm;
+  let mathInterestRate = (apr / 100) / c;
+  let interestOne =  1 + mathInterestRate;
+  var demoninator = (1 - Math.pow(interestOne, -n*c))/(mathInterestRate);
+  var resultantValue = Math.floor(p / demoninator);
   var resultNode = document.querySelector("#result");
   resultNode.innerHTML = `$${resultantValue}`;
+  return resultantValue;
 }
 
 function reset() {
@@ -24,20 +34,63 @@ function reset() {
   resultant_val.innerHTML = "";
 }
 
-function ammortisationSchedule() {
-// 500,000 * 0.002991666667 = 1,495.8333335 --> after the first month
-// 500,000 + 1,495.8333 = 501,495.8333  ---> interest added after the first month
-// 501,495 - 2,000 = 499,495  --> repayment deducted
+function ammortisation_schedule() {
   let ammortisedArray = [];
-  var loan_interest;
-  var ammortisedAmount;
-  let presentValue = Number(document.querySelector("#presentValue").value);
-  let interestRate = Number(document.querySelector("#interestRate").value);
-  let loanTerm = Number(document.querySelector("#loanTerm").value);
-  for(let i = 0; i < loanTerm; i++) {
-   loan_interest = presentValue * interestOne;
-   ammortisedAmount = presentValue - loan_interest;
-   ammortisedArray.push(ammortisedAmount);
+  let counter = [];
+  let fetch_data = getData();
+  var remainingBalance = fetch_data.presentValue;
+  let payment = calculate();
+  let term = fetch_data.loanTerm * fetch_data.compoundingPeriods;
+  let interestRate = fetch_data.interestRate / 100;
+  mathInterestRate = ((interestRate)/fetch_data.compoundingPeriods);
+  let interestByOne = 1 + mathInterestRate;
+  for(let i = 0; i < term; i++) {
+    interestComponent = remainingBalance * mathInterestRate
+    principalComponent = payment - interestComponent;
+    remainingBalance -= principalComponent;
+    ammortisedArray.push(Math.floor(remainingBalance));
+    counter.push(i);
   }
-  return ammortisedArray;
+  return [ammortisedArray, counter];
+};
+
+var configuration = {
+  type: 'line',
+  data: {
+      labels: ammortisation_schedule()[1],
+      datasets: [{
+          backgroundColor: 'rgba(2,83,179,0.2)',
+          label: "Balance Remaining",
+          data: ammortisation_schedule()[0]
+      },]
+  },
+    options: {
+      title: {
+        text: 'Breakdown of Payments'
+      },
+      scales: {
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Month'
+          },
+          gridLines: {
+            display: false
+          }
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Balance Remaining'
+          }
+        }]
+      },
+    }
+}
+
+function chart_run() {
+  var ctx = document.getElementById('myChart');
+  console.log(ammortisation_schedule()[1]);
+  console.log(ammortisation_schedule()[0]);
+  var myChart = new Chart(ctx, configuration);
 }
